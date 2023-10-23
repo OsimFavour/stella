c16a65438d0d976fe145f657ac04a12b9a56d9fc
OsimFavour <famos204@gmail.com>
2023-10-23 05:54:32 +0100

    Add Immer Reducer, states, dispatch, auth with djoser

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useImmerReducer } from 'use-immer'
 
 // MUI
 import { 
@@ -19,13 +20,67 @@ import {
 function Login() {
   const navigate = useNavigate()
 
-  return (
+  const initialState = {
+    usernameValue: '',
+    passwordValue: '',
+    sendRequest: 0
+    }
+
+    function ReducerFunction(draft, action) {
+        switch(action.type) {
+            case 'catchUsernameChange':
+                draft.usernameValue = action.usernameChosen
+                break
+            case 'catchPasswordChange':
+                draft.passwordValue = action.passwordChosen
+                break
+            // case 'changeSendRequest':
+            //     draft.sendRequest = draft.sendRequest + 1
+            //     break
+        }
+    }
+
+    // const [state, dispatch] = useReducer(ReducerFunction, initialState)
+    const [state, dispatch] = useImmerReducer(ReducerFunction, initialState)
+
+
+    function formSubmit(e) {
+        e.preventDefault()
+        console.log('The form has been submitted')
+        // dispatch({type: 'changeSendRequest'})
+    }
+
+
+    useEffect(() => {
+        if (state.sendRequest) {
+            const source = Axios.CancelToken.source()
+            async function SignIn() {
+                try {
+                    let url = 'http://localhost:8000/api-auth-djoser/token/login'
+                    const response = await Axios.post(url, {
+                        username: state.usernameValue,
+                        password: state.passwordValue,
+                    }, 
+                    { cancelToken: source.token })
+                    console.log(response)
+                    navigate('/')
+                }
+                catch (error) {
+                    console.log(error.response)
+                }
+            }
+            SignIn()
+            return () => {source.cancel()}
+            }
+    }, [state.sendRequest])
+
+    return (
     <div style={{ 
         width: '50%',
         marginLeft: 'auto',
         marginRight: 'auto',
         border: '5px solid white' }}>
-        <form>
+        <form onSubmit={formSubmit}>
             <Grid item container justifyContent='center' sx={{ marginTop: '1rem' }}>
                 <Typography variant='h4'>Sign In Here</Typography>
             </Grid>

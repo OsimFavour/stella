548e2340214ed5cb84611804ca6448a071c3e9a8
OsimFavour <famos204@gmail.com>
2023-10-26 22:15:52 +0100

    Update Property Forms

--- frontend/src/Components/AddProperty.js
+++ frontend/src/Components/AddProperty.js
@@ -75,20 +75,60 @@ function AddProperty() {
 				draft.longitudeValue = action.longitudeChosen
 				break
 
+			case 'catchPropertyStatusChange':
+				draft.propertyStatusValue = action.propertyStatusChosen
+				break
+
 			case 'catchPriceChange':
 				draft.priceValue = action.priceChosen
 				break
 
-			case 'catchPropertyStatusChange':
-				draft.propertyStatusValue = action.propertyStatusChosen
+			case 'catchRentalFrequencyChange':
+				draft.rentalFrequencyValue = action.rentalFrequencyChosen
+				break
+			
+			case 'catchRoomsChange':
+				draft.roomsValue = action.roomsChosen
 				break
 
-			case 'catchPropertyStatusChange':
-				draft.propertyStatusValue = action.propertyStatusChosen
+			case 'catchFurnishedChange':
+				draft.furnishedValue = action.furnishedChosen
 				break
 
-			case 'catchPropertyStatusChange':
-				draft.propertyStatusValue = action.propertyStatusChosen
+			case 'catchPoolChange':
+				draft.poolValue = action.poolChosen
+				break
+
+			case 'catchElevatorChange':
+				draft.elevatorValue = action.elevatorChosen
+				break
+
+			case 'catchCctvChange':
+				draft.cctvValue = action.cctvChosen
+				break
+			
+			case 'catchParkingChange':
+				draft.parkingValue = action.parkingChosen
+				break
+
+			case 'catchPicture1Change':
+				draft.picture1Value = action.picture1Chosen
+				break
+
+			case 'catchPicture2Change':
+				draft.picture2Value = action.picture2Chosen
+				break
+
+			case 'catchPicture3Change':
+				draft.picture3Value = action.picture3Chosen
+				break
+				
+			case 'catchPicture4Change':
+				draft.picture4Value = action.picture4Chosen
+				break
+
+			case 'catchPicture5Change':
+				draft.picture5Value = action.picture5Chosen
 				break
         }
     }
@@ -117,10 +157,122 @@ function AddProperty() {
 					<TextField 
 						id="title" 
 						label="Title" 
-						variant="outlined" 
+						variant="standard" 
 						fullWidth
 						value={state.titleValue}
 						onChange={(e) => dispatch({type: 'catchTitleChange', titleChosen: e.target.value})}/>
+				</Grid>
+
+				<Grid item container sx={{ marginTop: '1rem' }}>
+					<TextField 
+						id="ListingType" 
+						label="Listing Type" 
+						variant="standard" 
+						fullWidth
+						value={state.listingTypeValue}
+						onChange={(e) => 
+							dispatch({
+								type: 'catchListingTypeChange', 
+								listingTypeChosen: e.target.value
+								})
+							}
+					/>
+				</Grid>
+
+				<Grid item container sx={{ marginTop: '1rem' }}>
+					<TextField 
+						id="description" 
+						label="Description" 
+						variant="standard" 
+						fullWidth
+						value={state.descriptionValue}
+						onChange={(e) => 
+							dispatch({
+								type: 'catchDescriptionChange', 
+								descriptionChosen: e.target.value
+								})
+							}
+					/>
+				</Grid>
+
+				<Grid item container sx={{ marginTop: '1rem' }}>
+					<TextField 
+						id="area" 
+						label="Area" 
+						variant="standard" 
+						fullWidth
+						value={state.areaValue}
+						onChange={(e) => 
+							dispatch({
+								type: 'catchAreaChange', 
+								areaChosen: e.target.value
+								})
+							}
+					/>
+				</Grid>
+
+				<Grid item container sx={{ marginTop: '1rem' }}>
+					<TextField 
+						id="borough" 
+						label="Borough" 
+						variant="standard" 
+						fullWidth
+						value={state.boroughValue}
+						onChange={(e) => 
+							dispatch({
+								type: 'catchBoroughChange', 
+								boroughChosen: e.target.value
+								})
+							}
+					/>
+				</Grid>
+
+				<Grid item container sx={{ marginTop: '1rem' }}>
+					<TextField 
+						id="propertyStatus" 
+						label="Property Status" 
+						variant="standard" 
+						fullWidth
+						value={state.propertyStatusValue}
+						onChange={(e) => 
+							dispatch({
+								type: 'catchPropertyStatusChange', 
+								propertyStatusChosen: e.target.value
+								})
+							}
+					/>
+				</Grid>
+
+				<Grid item container sx={{ marginTop: '1rem' }}>
+					<TextField 
+						id="price" 
+						label="Price" 
+						variant="standard" 
+						fullWidth
+						value={state.priceValue}
+						onChange={(e) => 
+							dispatch({
+								type: 'catchPriceChange', 
+								priceChosen: e.target.value
+								})
+							}
+					/>
+				</Grid>
+
+				<Grid item container sx={{ marginTop: '1rem' }}>
+					<TextField 
+						id="rentalFrequency" 
+						label="Rental Frequency" 
+						variant="standard" 
+						fullWidth
+						value={state.rentalFrequencyValue}
+						onChange={(e) => 
+							dispatch({
+								type: 'catchrentalFrequencyChange', 
+								rentalFrequencyChosen: e.target.value
+								})
+							}
+					/>
 				</Grid>
 	
 				<Grid item container xs={8} sx={{ 

686946dd87bb93c0aa3661c109c6255467d33689
OsimFavour <famos204@gmail.com>
2023-11-02 11:19:33 +0100

    Update Listing serializers

--- rest_api/listings/api/serializers.py
+++ rest_api/listings/api/serializers.py
@@ -3,6 +3,19 @@ from ..models import Listing
 
 
 class ListingSerializer(serializers.ModelSerializer):
+    country = serializers.SerializerMethodField()
+    seller_username = serializers.SerializerMethodField()
+    seller_agency_name = serializers.SerializerMethodField()
+
+    def get_seller_agency_name(self, obj):
+        return obj.seller.profile.agency_name
+
+    def get_seller_username(self, obj):
+        print(f'Seller Property: {obj.seller}\n Seller Username: {obj.seller}')
+        return obj.seller.username
+    
+    def get_country(self, obj):
+        return 'Nigeria'
     class Meta:
         model = Listing
         fields = '__all__'

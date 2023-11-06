fef641dafb9dede13bc8805b832385232a2f078e
OsimFavour <famos204@gmail.com>
2023-11-06 15:28:55 +0100

    Add seller listings

--- rest_api/users/api/serializers.py
+++ rest_api/users/api/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from ..models import Profile
+from listings.models import Listing
+from listings.api.serializers import ListingSerializer
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    seller_listings = serializers.SerializerMethodField()
+
+    def get_seller_listings(self, obj):
+        query = Listing.objects.filter(seller=obj.seller)
+        serialize_listings = ListingSerializer(query, many=True)
+        return serialize_listings.data
+    
     class Meta:
         model = Profile
         fields = '__all__'

2a0f0d88155391ee88d869760a4829b757a88260
OsimFavour <famos204@gmail.com>
2023-11-16 16:58:13 +0100

    Add Snackbar Notifications

--- frontend/src/Components/ListingUpdate.js
+++ frontend/src/Components/ListingUpdate.js
@@ -19,8 +19,9 @@ import {
   CardContent,
   CircularProgress,
   TextField,
-	FormControlLabel,
-	Checkbox,
+  FormControlLabel,
+  Checkbox,
+  Snackbar
 } from '@mui/material'
 
 
@@ -96,57 +97,72 @@ function ListingUpdate(props) {
 		cctvValue: props.listingData.cctv,
 		parkingValue: props.listingData.parking,
 		sendRequest: 0,
+		openSnack: false,
+		disabledBtn: false,
+		
   } 
 
   function ReducerFunction(draft, action) {
     switch(action.type) {
-      case 'catchTitleChange':
-        draft.titleValue = action.titleChosen
-        break
+		case 'catchTitleChange':
+			draft.titleValue = action.titleChosen
+			break
 
-      case 'catchListingTypeChange':
-        draft.listingTypeValue = action.listingTypeChosen
-        break
+		case 'catchListingTypeChange':
+			draft.listingTypeValue = action.listingTypeChosen
+			break
 
-      case 'catchDescriptionChange':
-        draft.descriptionValue = action.descriptionChosen
-        break
+		case 'catchDescriptionChange':
+			draft.descriptionValue = action.descriptionChosen
+			break
 
-      case 'catchPropertyStatusChange':
-        draft.propertyStatusValue = action.propertyStatusChosen
-        break
+		case 'catchPropertyStatusChange':
+			draft.propertyStatusValue = action.propertyStatusChosen
+			break
 
-      case 'catchPriceChange':
-        draft.priceValue = action.priceChosen
-        break
+		case 'catchPriceChange':
+			draft.priceValue = action.priceChosen
+			break
 
-      case 'catchRentalFrequencyChange':
-        draft.rentalFrequencyValue = action.rentalFrequencyChosen
-        break
+		case 'catchRentalFrequencyChange':
+			draft.rentalFrequencyValue = action.rentalFrequencyChosen
+			break
       
-      case 'catchRoomsChange':
-        draft.roomsValue = action.roomsChosen
-        break
+		case 'catchRoomsChange':
+			draft.roomsValue = action.roomsChosen
+			break
 
-      case 'catchFurnishedChange':
-        draft.furnishedValue = action.furnishedChosen
-        break
+		case 'catchFurnishedChange':
+			draft.furnishedValue = action.furnishedChosen
+			break
 
-      case 'catchPoolChange':
-        draft.poolValue = action.poolChosen
-        break
+		case 'catchPoolChange':
+			draft.poolValue = action.poolChosen
+			break
 
-      case 'catchElevatorChange':
-        draft.elevatorValue = action.elevatorChosen
-        break
+		case 'catchElevatorChange':
+			draft.elevatorValue = action.elevatorChosen
+			break
 
-      case 'catchCctvChange':
-        draft.cctvValue = action.cctvChosen
-        break
+		case 'catchCctvChange':
+			draft.cctvValue = action.cctvChosen
+			break
 
-      case 'changeSendRequest':
-        draft.sendRequest = draft.sendRequest + 1
-        break
+		case 'changeSendRequest':
+			draft.sendRequest = draft.sendRequest + 1
+			break
+
+		case 'setSnack':
+			draft.openSnack = true
+			break
+
+		case 'disableButton':
+			draft.disabledBtn = true
+			break
+
+		case 'enableButton':
+			draft.disabledBtn = false
+			break
 
     }
   }
@@ -161,6 +177,7 @@ function ListingUpdate(props) {
 		// Make Request when The Send Request is Incremented 
 		// i.e. when the submit button is clicked
         dispatch({type: 'changeSendRequest'})
+        dispatch({type: 'disableButton'})
     }
 
 	// SEND REQUESTS
@@ -170,55 +187,69 @@ function ListingUpdate(props) {
 				const formData = new FormData()
 				// Here we will append key value pairs
 
-        if (state.listingTypeValue === 'Office') {
-          // If the value is updated to Office, the rooms
-          // value shouldn't be appended anymore. Instead, 
-          // the value should be zero
-
-          formData.append('title', state.titleValue)
-          formData.append('description', state.descriptionValue)
-          formData.append('listing_type', state.listingTypeValue)
-          formData.append('property_status', state.propertyStatusValue)
-          formData.append('price', state.priceValue)
-          formData.append('rental_frequency', state.rentalFrequencyValue)
-          formData.append('rooms', 0)
-          formData.append('furnished', state.furnishedValue)
-          formData.append('pool', state.poolValue)
-          formData.append('elevator', state.elevatorValue)
-          formData.append('cctv', state.cctvValue)
-          formData.append('parking', state.parkingValue) 
-          formData.append('seller', GlobalState.userId)
-        }
-        else {
-          formData.append('title', state.titleValue)
-          formData.append('description', state.descriptionValue)
-          formData.append('listing_type', state.listingTypeValue)
-          formData.append('property_status', state.propertyStatusValue)
-          formData.append('price', state.priceValue)
-          formData.append('rental_frequency', state.rentalFrequencyValue)
-          formData.append('rooms', state.roomsValue)
-          formData.append('furnished', state.furnishedValue)
-          formData.append('pool', state.poolValue)
-          formData.append('elevator', state.elevatorValue)
-          formData.append('cctv', state.cctvValue)
-          formData.append('parking', state.parkingValue) 
-          formData.append('seller', GlobalState.userId)
-        }
+				if (state.listingTypeValue === 'Office') {
+				// If the value is updated to Office, the rooms
+				// value shouldn't be appended anymore. Instead, 
+				// the value should be zero
+					formData.append('title', state.titleValue)
+					formData.append('description', state.descriptionValue)
+					formData.append('listing_type', state.listingTypeValue)
+					formData.append('property_status', state.propertyStatusValue)
+					formData.append('price', state.priceValue)
+					formData.append('rental_frequency', state.rentalFrequencyValue)
+					formData.append('rooms', 0)
+					formData.append('furnished', state.furnishedValue)
+					formData.append('pool', state.poolValue)
+					formData.append('elevator', state.elevatorValue)
+					formData.append('cctv', state.cctvValue)
+					formData.append('parking', state.parkingValue) 
+					formData.append('seller', GlobalState.userId)
+				}
+				else {
+					formData.append('title', state.titleValue)
+					formData.append('description', state.descriptionValue)
+					formData.append('listing_type', state.listingTypeValue)
+					formData.append('property_status', state.propertyStatusValue)
+					formData.append('price', state.priceValue)
+					formData.append('rental_frequency', state.rentalFrequencyValue)
+					formData.append('rooms', state.roomsValue)
+					formData.append('furnished', state.furnishedValue)
+					formData.append('pool', state.poolValue)
+					formData.append('elevator', state.elevatorValue)
+					formData.append('cctv', state.cctvValue)
+					formData.append('parking', state.parkingValue) 
+					formData.append('seller', GlobalState.userId)
+				}
 
 				try {
 					let url = `http://localhost:8000/api/listings/${props.listingData.id}/update/`
 					const response = await Axios.patch(url, formData)
 					console.log(response.data)
-          // To make the detail listing page refresh
-					navigate(0)
+					dispatch({
+                        type: 'setSnack'
+                    })
+					// To make the detail listing page refresh
+					// navigate(0)
 				}
 				catch(e) {
 					console.log(e.response)
+					dispatch({type: 'enableButton'})
 				}
 			}
+
 			UpdateProperty()
 		}
 	}, [state.sendRequest])
+
+	// CREATE A NEW USE EFFECT TO WATCH FOR CHANGES IN OPEN SNACK
+    useEffect(() => {
+        if (state.openSnack){
+            // SET A TIMEOUT FOR 0.5 SEC AND DO A REDIRECT
+            setTimeout(() => {
+                navigate(0)
+            }, 500)
+        }
+    }, [state.openSnack]) 
 
 
 	function PriceDisplay() {
@@ -486,14 +517,22 @@ function ListingUpdate(props) {
 					marginLeft: 'auto',
 					marginRight: 'auto',
 					fontSize: '1.1rem' }}>
-					<Button variant='contained' type='submit' fullWidth>Update</Button>
+					<Button variant='contained' type='submit' fullWidth disabled={state.disabledBtn}>Update</Button>
 					{/* <Button variant='contained' type='submit' fullWidth>Submit</Button> */}
 				</Grid>
 	 	   
 			</form>
 
-      <Button variant='contained' onClick={props.closeDialog}>Cancel</Button>
+      		<Button variant='contained' onClick={props.closeDialog}>Cancel</Button>
 
+			  <Snackbar
+					open={state.openSnack}
+					message="Listing Updated Successfully!"
+					anchorOrigin={{
+						vertical: 'top',
+						horizontal: 'center',
+					}}
+				/>
 			{/* <Button onClick={() => state.mapInstance.flyTo(
 				[51.5318314686333, -0.07821153682234076],
 				15
